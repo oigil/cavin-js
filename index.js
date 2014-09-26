6b59412449ff0c886d6f8d0bca403cb8ea6f8c90
조규태 <oigil@daumcorp.com>
2014-09-26 10:36:53 +0900

    init.

---
+++ index.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+
+var cavinTxt = fs.readFileSync("./resources/cavin-1.txt", "utf8");
+console.log(cavinTxt);
+console.log("");
+console.log("Hello! I'm Cavin.js of daumkakao.");
+console.log("Mail me to 'oigil.jo@gmail.com' or 'cavin.js@daumkakao.com'");
+console.log("");
